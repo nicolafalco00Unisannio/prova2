87c6d7263760a883260ef7994c51c852d2b5fcf1
Falco <Falco@Falco-PC>
2022-04-05 15:09:03 +0200

    Prova commit 3

---
+++ prova2/src/main/java/it/nfalco/prova2/App.java
@@ -0,0 +1,14 @@
+package it.nfalco.prova2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Orcodio!");
+    }
+}
